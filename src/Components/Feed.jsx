63e60eff27cd8761e04a7ddaef991141bd650635
Prachi <prachi807770@gmail.com>
2024-09-20 18:31:14 +0530

    social-media-feed-aggregator

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,6 +1,16 @@
-import { Button } from "@mui/material";
+import { Button, Stack, Step } from "@mui/material";
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { CiHeart } from "react-icons/ci";
+import { CiBookmark } from "react-icons/ci";
+
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  Container,
+} from "@mui/material";
 
 const Feed = (props) => {
   const navigate = useNavigate();
@@ -12,7 +22,31 @@ const Feed = (props) => {
 
   return (
     <>
-      <div>i m feed</div>
+      <div>
+        <Container>
+          <h1>Instagram Postcard</h1>
+          <Card sx={{ maxWidth: 345, margin: "auto", boxShadow: 3 }}>
+            <CardContent>
+              <Typography variant="h6" component="div" gutterBottom>
+                username_here
+              </Typography>
+            </CardContent>
+            <CardMedia
+              component="img"
+              height="240"
+              image="https://media.istockphoto.com/id/1412834907/photo/group-of-multiethnic-businesspeople-discussing-about-financial-strategy-renewable-power.jpg?s=1024x1024&w=is&k=20&c=kdMm22YmQwXzhs-Ut5GIxOK1g5dJi8MGIgS9Ip5E5gc=" // Replace with your image URL
+              alt="Post"
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary"></Typography>
+              <Stack direction={"row"} justifyContent={"space-between"}>
+                <CiHeart />
+                <CiBookmark />
+              </Stack>
+            </CardContent>
+          </Card>
+        </Container>
+      </div>
       <Button onClick={handleLogout}>Logout</Button>
     </>
   );
